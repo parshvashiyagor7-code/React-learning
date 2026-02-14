9facedda51d8153cdf317381c88f31e1378f70c1
parshvashiyagor7-code <parshvashiyagor7@gmail.com>
2026-02-14 13:42:08 +0530

    just basic learning

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,15 @@
+const App = () => {
+  return (
+    <>
+    <div id="parent">
+      <h1>Hello</h1>
+      <h3>My name is Parshva</h3>
+    </div>
+    <div id="chacha">
+      
+    </div>
+    </>
+  )
+}
+
+export default App
